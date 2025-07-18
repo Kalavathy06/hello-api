521ce0325728f6b6fa8c11abd2a27a369dfdc16b
Kalavathy K <kaviyakalavathy@gmail.com>
2025-07-18 13:12:42 +0530

    fix: changed name for PR trigger

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,5 +9,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: "John Doe" });//testing pr visibility
+  res.status(200).json({ name: "Kalavathy" });//testing pr visibility
 }
